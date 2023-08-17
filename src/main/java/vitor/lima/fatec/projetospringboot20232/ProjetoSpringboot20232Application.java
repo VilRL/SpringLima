fd8a9683c3187cc5a1e46d89eec8283e8f89624c
Vitor Lima <vitor02office@gmail.com>
2023-08-16 21:02:19 -0300

    Meu primeiro commit nesse projeto

---
+++ src/main/java/vitor/lima/fatec/projetospringboot20232/ProjetoSpringboot20232Application.java
@@ -0,0 +1,13 @@
+package vitor.lima.fatec.projetospringboot20232;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringboot20232Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringboot20232Application.class, args);
+	}
+
+}
---
+++ src/test/java/vitor/lima/fatec/projetospringboot20232/ProjetoSpringboot20232ApplicationTests.java
@@ -0,0 +1,13 @@
+package vitor.lima.fatec.projetospringboot20232;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringboot20232ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
